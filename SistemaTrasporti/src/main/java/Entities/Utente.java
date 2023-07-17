ad6b20702730836df998cfe68bff5519986125c9
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-07-17 16:43:04 +0200

    Implementazione Utente Tessera

--- SistemaTrasporti/src/main/java/Entities/Utente.java
+++ SistemaTrasporti/src/main/java/Entities/Utente.java
@@ -1,5 +1,36 @@
 package Entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
 public class Utente {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String Nome;
+	private String Cognome;
+	private Tessera Tessera;
+	
+	public Utente(String _nome, String _cognome, Tessera _tessera) {
+		this.Nome = _nome;
+		this.Cognome = _cognome;
+		this.Tessera = _tessera;
+	}
+	
+	@OneToOne
+	private Tessera tessera;
 
 }
