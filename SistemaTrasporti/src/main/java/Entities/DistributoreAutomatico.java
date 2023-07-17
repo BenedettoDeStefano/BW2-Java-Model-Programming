367b7124f489dced299ee97d364f410876e9c862
Gustavo Oliveira <123893190+Gust4voo@users.noreply.github.com>
2023-07-17 17:30:29 +0200

    test

--- SistemaTrasporti/src/main/java/Entities/DistributoreAutomatico.java
+++ SistemaTrasporti/src/main/java/Entities/DistributoreAutomatico.java
@@ -1,5 +1,30 @@
 package Entities;
 
-public class DistributoreAutomatico {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class DistributoreAutomatico {
+	@Id
+	@GeneratedValue
+   Long id;
+   String posizione;
+   Boolean attivo;
+public DistributoreAutomatico(String posizione, Boolean attivo) {
+	super();
+	this.posizione = posizione;
+	this.attivo = attivo;
+}
+   
+  
 }
