06956a93e745508dfb36c61e4ff857abfd48e3ab
Benedetto <129066047+BenedettoDeStefano@users.noreply.github.com>
2023-07-18 17:48:48 +0200

    metodi officinaDAO

--- SistemaTrasporti/src/main/java/DAO/BigliettoDAO.java
+++ SistemaTrasporti/src/main/java/DAO/BigliettoDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
 import org.jboss.logging.Logger;
 
@@ -19,23 +20,30 @@ public class BigliettoDAO {
         this.em = em;
     }
     
+    
+    // Emissione Biglietto
     public void emettiBiglietto(Biglietto biglietto) {
     	EntityTransaction t = em.getTransaction();
-        t.begin();
-
-        em.persist(biglietto);
-
-        t.commit();
-        log.info("Biglietto emesso correttamente");
-     }
-    
+    	try {
+            t.begin();
+            em.persist(biglietto);
+            t.commit();
+            log.info("Biglietto emesso correttamente");
+        } catch (Exception e) {
+            t.rollback();
+            log.error("Errore durante l'emissione del biglietto", e);
+        }
+    }
+     
+    // Numero biglietti vidimati
     public List<Biglietto> acquisisciNumeroBigliettiVidimati() {
         List<Biglietto> bigliettiVidimati = em.createQuery("SELECT b FROM Biglietto b WHERE b.vidimato = true", Biglietto.class)
               .getResultList();
         log.info("Numero biglietti vidimati: " + bigliettiVidimati.size());
         return bigliettiVidimati;
      }
-
+    
+    // Vidima biglietto
      public void vidimaBiglietto(Biglietto biglietto) {
     	 if (!biglietto.getVidimato()) {
     		 biglietto.setVidimato(true);
@@ -45,6 +53,54 @@ public class BigliettoDAO {
     		 log.info("Biglietto già vidimato in precedenza");
     	 }
         
+     }
+     
+     // Cerca biglietto con id
+     public Biglietto getBigliettoById(Long id) {
+    	    try {
+    	        Biglietto biglietto = em.find(Biglietto.class, id);
+    	        if (biglietto != null) {
+    	            log.info("Biglietto trovato con ID " + id);
+    	        }
+    	        return biglietto;
+    	    } catch (Exception e) {
+    	        log.error("Errore durante la ricerca del biglietto con ID " + id, e);
+    	        return null;
+    	    }
+    	}
+     
+     // COntrolla tutti i biglietti
+     public List<Biglietto> getAllBiglietti() {
+         Query query = em.createQuery("SELECT b FROM Biglietto b");
+         return query.getResultList();
+     }
+     
+     // Aggiorna biglietto
+     public void updateBiglietto(Biglietto biglietto) {
+    	 EntityTransaction t = em.getTransaction();
+         try {
+             t.begin();
+             em.merge(biglietto);
+             t.commit();
+             log.info("Biglietto aggiornato correttamente");
+         } catch (Exception e) {
+             t.rollback();
+             log.error("Errore durante l'aggiornamento del biglietto", e);
+         }
+     }
+     
+     // Elimina biglietto
+     public void deleteBiglietto(Biglietto biglietto) {
+    	 EntityTransaction t = em.getTransaction();
+         try {
+             t.begin();
+             em.remove(biglietto);
+             t.commit();
+             log.info("Biglietto eliminato correttamente");
+         } catch (Exception e) {
+             t.rollback();
+             log.error("Errore durante l'eliminazione del biglietto", e);
+         }
      }
 
 }
--- SistemaTrasporti/src/main/java/DAO/OfficinaDAO.java
+++ SistemaTrasporti/src/main/java/DAO/OfficinaDAO.java
@@ -1,8 +1,13 @@
 package DAO;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.jboss.logging.Logger;
+
+import Entities.Officina;
 
 public class OfficinaDAO {
 	
@@ -11,6 +16,54 @@ public class OfficinaDAO {
 
     public OfficinaDAO(EntityManager em) {
         this.em = em;
+    }
+    
+    // Salva Officina
+    public void saveOfficina(Officina officina) {
+        em.getTransaction().begin();
+        
+        em.persist(officina);
+        
+        em.getTransaction().commit();
+        log.info("Officina salvata correttamente");
+    }
+    
+    // Cerca officina id
+    public Officina getOfficinaById(Long id) {
+        try {
+            Officina officina = em.find(Officina.class, id);
+            if (officina != null) {
+                log.info("Officina trovata con ID " + id);
+            }
+            return officina;
+        } catch (Exception e) {
+            log.error("Errore durante la ricerca dell'officina con ID " + id, e);
+            return null;
+        }
+    }
+    
+    // Cerca tutte le officine
+    public List<Officina> getAllOfficine() {
+        Query query = em.createQuery("SELECT o FROM Officina o");
+        return query.getResultList();
+    }
+    // Aggiorna officina
+    public void updateOfficina(Officina officina) {
+        em.getTransaction().begin();
+        
+        em.merge(officina);
+        
+        em.getTransaction().commit();
+        log.info("Officina aggiornata correttamente");
+    }
+    // Elimina Officina
+    public void deleteOfficina(Officina officina) {
+        em.getTransaction().begin();
+        
+        em.remove(officina);
+        
+        em.getTransaction().commit();
+        log.info("Officina eliminata correttamente");
     }
 
 }
