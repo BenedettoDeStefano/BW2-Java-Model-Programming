7df4cac9aababb23db1241087c68ddf798231071
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-07-18 17:28:43 +0200

    aggiunto metodo salvaTessera in tesseraDAO

--- SistemaTrasporti/src/main/java/DAO/TesseraDAO.java
+++ SistemaTrasporti/src/main/java/DAO/TesseraDAO.java
@@ -1,16 +1,43 @@
 package DAO;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
 import org.jboss.logging.Logger;
 
+import Entities.Tessera;
+
 public class TesseraDAO {
 	
-	private final EntityManager em;
-    private static Logger log = Logger.getLogger(TesseraDAO.class);
-
-    public TesseraDAO(EntityManager em) {
-        this.em = em;
-    }
-
+	private EntityManagerFactory emf;
+	
+	public TesseraDAO() {
+		emf = Persistence.createEntityManagerFactory("sistemaTrasporti");
+	}
+	
+	public void salvaTessera(Tessera tessera) {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction transaction = null;
+		
+		try {
+			transaction = em.getTransaction();
+			transaction.begin();
+			
+			em.persist(tessera);
+			
+			transaction.commit();
+			
+			System.out.println("Tessera salvata nel databse correttamente");
+		}catch (Exception e){
+			if (transaction != null) 
+				transaction.rollback();
+			
+			System.out.println("Errore durante salvataggio della tessera!");
+		}finally {
+			if (em != null)
+				em.close();
+		}
+	}
 }
